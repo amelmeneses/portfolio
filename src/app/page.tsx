6021f15651012e52f225e8c72331e57bb2e9300e
Ame <ame@MacBook-Pro-de-Ame.local>
2025-01-21 12:35:21 -0500

    About-me component and deleted react svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import AboutMe from "../components/AboutMe";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <AboutMe />
     </>
   );
 }
